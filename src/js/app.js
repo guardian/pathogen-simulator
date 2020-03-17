95c409fcbc35d26997e5bbc26dbfd22f6167e8e8
jerseywonder <work@andyball.info>
2020-03-18 09:51:04 +1100

    git status Remove population slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,12 @@
-import config from './settings/settings'
-import { Preflight } from './modules/preflight'
-
-//import ScrollyTeller from "../modules/scrollyteller"
-//import scrollTriggers from "../modules/scrollTriggers.js";
 import loadJson from '../components/load-json/'
 import { $, $$, round, numberWithCommas, wait, getDimensions } from './modules/util'
 
 import rzero from './modules/rzero'
-//import multiples from './modules/small-multiples'
 import pathogen from './modules/pathogen'
-
 import multiples from './modules/multiples'
+
+import config from './settings/settings'
+import { Preflight } from './modules/preflight'
 
 var settings = new Preflight(config).getSettings()
 
@@ -19,64 +15,3 @@ pathogen.init(settings.pathogen, settings.sliders, settings.cases)
 multiples.init()
 
 rzero.init()
-
-
-
-//import videoInview from "../modules/videoInview";
-//import imageInview from "../modules/imageInview";
-//import sticky from "../modules/sticky";
-//import shareable from "../modules/shareable";
-//import Lightbox from '../modules/lightbox'
-//import intro from '../modules/intro'
-//import Relocator from '../modules/relocator'
-//import drawMultilineText from 'canvas-multiline-text'
-//
-//import smoothscroll from 'smoothscroll-polyfill';
-//smoothscroll.polyfill();
-
-/*
-
-
-console.log(wrangle)
-
-var triggers = document.querySelectorAll('.trigger');
-
-var config = {
-
-  rootMargin: '0px 0px 0px 100%',
-
-  threshold: 0
-
-}
-
-let trigger = new IntersectionObserver(function(entries, exit) {
-
-  entries.forEach( (entry) => {
-
-    if (entry.isIntersecting) {
-
-        var id = +entry.target.dataset.id
-
-        //self.currentTrigger = id
-
-        console.log(id)
-
-    }
-
-  });
-
-}, config);
-
-triggers.forEach( (elem, index) => {
-
-  if (index < self.application.database.locations.length) {
-
-    elem.setAttribute('data-id', index);
-
-    trigger.observe(elem);
-
-  }
-
-});
-
-*/
